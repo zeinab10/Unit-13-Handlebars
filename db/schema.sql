1ec8d4fc98747c10caf7a1ea4e1795ef46fdebed
Zeinab <zeinab@Zeinabs-MacBook-Air.local>
2020-05-03 10:50:44 -0500

    add schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,9 @@
-CREATE DATABASE burgers_db;
+CREATE DATABASE burger_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-    id INT
-    AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR
-    (255) NOT NULL,
-    devoured BOOLEAN DEFAULT false, 
-    PRIMARY KEY
-    (id);
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (50) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
 );
---
+++ server.js
@@ -0,0 +1,3 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
