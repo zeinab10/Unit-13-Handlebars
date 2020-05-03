d76331dd960dad7e7b3196438b8d1a3e08b25e69
Zeinab <zeinab@Zeinabs-MacBook-Air.local>
2020-05-03 09:59:44 -0500

    files

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR
+    (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false, 
+    PRIMARY KEY
+    (id);
+);
