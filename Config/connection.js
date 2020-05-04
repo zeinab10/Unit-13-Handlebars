e1f342d0f973f3aaa98555f10926ef55e2df77ae
Zeinab <zeinab@Zeinabs-MacBook-Air.local>
2020-05-03 20:32:59 -0500

    changes

--- Config/connection.js
+++ Config/connection.js
@@ -1,11 +1,19 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "zj526670",
-    database:"burgers_db"
+
+govar connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "zj526670",
+        database:"burgers_db"
 });
+
+  }
 
 //Make connection
 
